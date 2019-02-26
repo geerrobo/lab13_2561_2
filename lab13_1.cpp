e90ff2184a26520a4019d317d80a96ef6175c6d3
lab303-23 <lab303-23@labeng.local>
2019-02-26 16:23:28 +0700

    1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -2,18 +2,43 @@
 #include <string>
 using namespace std;
 
+int pos;
+
 template <typename T>
 void swap(T d[],int x,int y){
     T temp = d[x];
     d[x] = d[y];
     d[y] = temp;
+	pos = x;
 }
 
 template <typename T>
 void insertionSort(T d[],int N){
-	
-//Write your code here.
-
+	int key, j; 
+   	for (int i = 1; i < N; i++){ 
+  	    key = d[i];
+		// show
+		for(int k=0;k<N;k++){
+			if(k==i)	cout << "[" << d[k] << "] ";
+			else	cout << d[k] << " ";
+		}		
+		// show
+		j = i-1;
+		pos = i;
+		while (j >= 0 && d[j] < key){ 
+        	swap(d,j,j+1);
+			j--;
+        } 
+        d[j+1] = key;
+		// show after swap
+		cout << " => ";
+		for(int k=0;k<N;k++){
+			if(k==pos)	cout << "[" << d[k] << "] ";
+			else	cout << d[k] << " ";
+		}
+		cout << "\n";
+		// show after swap
+    }
 }
 
 int main(){
